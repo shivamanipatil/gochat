475ae590c07109fa46c58faeebeb51c2e83e2054
Shivamani Patil <shivamanipatil10@gmail.com>
2020-04-25 13:25:18 +0530

    main.go: Can send message to all connections and receive to

--- server/main.go
+++ server/main.go
@@ -2,19 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
-func sum1(s1 chan int) {
-	sum := 0
-	for i := 0; i < 100000; i++ {
-		sum = sum + i
-	}
-	s1 <- sum
-}
+var connections []net.Conn
 
 func main() {
 	sigs := make(chan os.Signal, 1)
@@ -32,13 +27,11 @@ func main() {
 			if err != nil {
 				fmt.Println(err)
 			}
+			connections = append(connections, conn)
 			go handleConnection(conn)
 		}
 	}()
-
-	s1 := make(chan int, 1)
 	go readInput(msg)
-	go sum1(s1)
 loop:
 	for {
 		select {
@@ -46,11 +39,9 @@ loop:
 			fmt.Println("Exit:")
 			break loop
 		case s := <-msg:
-			fmt.Println(s)
-		case i := <-s1:
-			fmt.Println(i)
+			fmt.Println("Current connections are: ", connections)
+			sendToAllConnections(connections, s)
 		}
-
 	}
 }
 
@@ -69,8 +60,40 @@ func handleConnection(conn net.Conn) {
 	for {
 		n, err := conn.Read(buff)
 		if err != nil {
+			if err == io.EOF {
+				connections = removeConn(connections, conn)
+				fmt.Println(conn, " Disconnected")
+				fmt.Println("COnnections : ", connections)
+				break
+			}
 			fmt.Println(err)
 		}
-		fmt.Println("THis is the text : ", string(buff[:n]))
+		fmt.Println(string(buff[:n]))
 	}
+}
+
+func sendToAllConnections(connections []net.Conn, msg string) {
+	for _, conn := range connections {
+		b := []byte(msg)
+		n, err := conn.Write(b)
+		if err != nil {
+			fmt.Println("Couldnt write to : ", conn)
+		}
+		if n != len(b) {
+			fmt.Println("Could'nt write whole message")
+		}
+	}
+}
+
+func removeConn(connections []net.Conn, conn net.Conn) []net.Conn {
+	j := 0
+	q := make([]net.Conn, len(connections))
+	for _, n := range connections {
+		if n != conn {
+			q[j] = n
+			j++
+		}
+	}
+	q = q[:j]
+	return q
 }
